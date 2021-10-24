c2950ce74dc8b5d194312187c915350e0244bee4
Yan Figueiredo <yancfm@gmail.com>
2021-10-24 20:19:44 -0300

    bfs is working 😎😎😎😎😎😎
    i rewrote everything

--- week-3/algorithms/search.py
+++ week-3/algorithms/search.py
@@ -1,231 +1,45 @@
+from state import State, traceback_board
 from collections import deque
 
 
-# auxiliar functions
-def get(table, size, i, j):
-    return table[i * size + j]
+def bfs(initial_state):
 
-def position(table, size, index):
+    # Performance
+    max_depth       = 0
+    max_frontier    = 0
+    scanned         = 0
 
-    i = index // size
+    # Create a queue for the states to be explored
+    queue = deque()
+    explored = set()
 
-    return i, index - i * size
+    # Add the initial state to the queue
+    queue.append(initial_state)
 
-
-# class methods
-class Node:
-
-    def __init__(self, table, size, parent, depth, move):
-        self.table = table
-        self.size = size
-        self.parent = parent
-        self.depth = depth
-        self.move = move
-
-
-    def show(self):
-        return ','.join(str(i) for i in self.table)
-
-
-    def __eq__(self, other):
-
-        try:
-            for i in range(len(self.table)):
-                if self.table[i] != other.table[i]:
-                    return False
-            return True
-        
-        except:
-            return False
-    
-    def __str__(self):
-
-        s = ""
-        for i in range(self.size):
-            for j in range(self.size):
-                s += str(get(self.table, self.size, i, j)) + " "
-            s += "\n"
-        
-        return s
-
-
-def children_generator(node):
-
-    children = []
-    moves = {'up': (0, 1), 'down': (0, -1), 'left': (-1, 0), 'right': (1, 0)}
-    
-    # position of 0
-    index = node.table.index(0)
-    i, j = position(node.table, node.size, index)
-
-    for key, value in moves.items():
-        i_t = i + value[0] # x
-        j_t = j + value[1] # y
-
-        if (i_t >= 0 and i_t < node.size) and (j_t >= 0 and j_t < node.size): 
-
-            new_table = node.table.copy()
-            
-            p_t = i_t * node.size + j_t # temp position
-            new_table[index], new_table[p_t] = new_table[p_t], new_table[index]
-            
-            # def __init__(self, table, size, parent, depth, move):
-            children.append(Node(new_table, node.size, node, node.depth + 1, key))
-
-    return children
-
-
-def bfs(initial_table, final_state, size):
-    initial = Node(initial_table, size, None, 0, "NoOp")
-
-    queue = deque([initial]) # Queue
-    viewed = set() # Set
-
-    print(initial.show())
-    print(final_state)
-
+    # While the queue is not empty
     while queue:
 
-        node = queue.popleft()
-        viewed.add(node.show())
+        # Remove the first state from the queue
+        current_state = queue.popleft()
+        explored.add(current_state.key)
 
-        # It's the goal?
-        if node.show() == final_state:
-            return node
-            
-        # It's not the goal :(
-        children = children_generator(node)
-
-        for child in children:
-            if child.show() not in viewed:
-                queue.append(child)
-                viewed.add(child.show())
+        # If the current state is the goal state
+        if current_state.goal():
+            return {"state": current_state, "max_depth": max_depth, "max_frontier": max_frontier, "final_frontier": len(queue), "scanned": scanned}
 
 
+        # Add the current state's children to the queue
+        neighbors = current_state.get_neighbors()
+        for neighbor in neighbors:
+            if neighbor.key not in explored:
+                scanned += 1
+                queue.append(neighbor)
 
-def print_children(children):
+                if neighbor.depth > max_depth:
+                    max_depth = neighbor.depth
 
-    for child in children:
-        print(child)
+        if len(queue) > max_frontier:
+            max_frontier = len(queue)
 
-
-if __name__ == "__main__":
-
-    print("direita-baixo")
-    node = Node([1, 2, 3,
-                4, 5, 6, 
-                7, 8, 0], 
-                3, None, 0, "NoOp")
-
-    print("Pai: ")
-    print(node)
-    print("Filhos: ")
-    print_children(children_generator(node))
-
-
-
-    print("esquerda-cima")
-    node = Node([0, 2, 3,
-                4, 5, 6, 
-                7, 8, 1], 
-                3, None, 0, "NoOp")
-
-    print("Pai: ")
-    print(node)
-    print("Filhos: ")
-    print_children(children_generator(node))
-
-
-
-    print("esquerda-baixo")
-    node = Node([7, 2, 3,
-                4, 5, 6, 
-                0, 8, 1], 
-                3, None, 0, "NoOp")
-
-    print("Pai: ")
-    print(node)
-    print("Filhos: ")
-    print_children(children_generator(node))
-
-
-
-    print("direita-cima")
-    node = Node([7, 2, 0,
-                4, 5, 6, 
-                3, 8, 1], 
-                3, None, 0, "NoOp")
-
-    print("Pai: ")
-    print(node)
-    print("Filhos: ")
-    print_children(children_generator(node))
-
-
-
-    print("centro-baixo")
-    node = Node([1, 2, 3,
-                4, 5, 6, 
-                7, 0, 8], 
-                3, None, 0, "NoOp")
-
-    print("Pai: ")
-    print(node)
-    print("Filhos: ")
-    print_children(children_generator(node))
-
-
-
-    print("centro-cima")
-    node = Node([2, 0, 3,
-                4, 5, 6, 
-                7, 8, 1], 
-                3, None, 0, "NoOp")
-
-    print("Pai: ")
-    print(node)
-    print("Filhos: ")
-    print_children(children_generator(node))
-
-
-
-    print("centro-esquerda")
-    node = Node([7, 2, 3,
-                0, 5, 6, 
-                4, 8, 1], 
-                3, None, 0, "NoOp")
-
-    print("Pai: ")
-    print(node)
-    print("Filhos: ")
-    print_children(children_generator(node))
-
-
-
-    print("centro-direita")
-    node = Node([7, 2, 6,
-                4, 5, 0, 
-                3, 8, 1], 
-                3, None, 0, "NoOp")
-
-    print("Pai: ")
-    print(node)
-    print("Filhos: ")
-    print_children(children_generator(node))
-
-
-
-    print("centro")
-    node = Node([7, 2, 6,
-                4, 0, 5, 
-                3, 8, 1], 
-                3, None, 0, "NoOp")
-
-    print("Pai: ")
-    print(node)
-    print("Filhos: ")
-    print_children(children_generator(node))
-
-
-    
-
+    # If the queue is empty and the goal state has not been found
+    return {"state": None, "max_depth": max_depth, "max_frontier": max_frontier, "final_frontier": len(queue), "scanned": scanned}
--- week-3/main.py
+++ week-3/main.py
@@ -1,7 +1,14 @@
+# System
 import sys
+import time
 
+# External
+import resource
+
+# Own
 from show import show
 from algorithms.search import *
+from state import State, traceback_board
 
 def create(input):
     table = [int(i) for i in input.split(',')]
@@ -17,18 +24,39 @@ def main():
 
     method      = sys.argv[1]
     table, size = create(sys.argv[2])
+    goal = [0, 1, 2, 3, 4, 5, 6, 7, 8]
+
+    goal_state = "".join(str(i) for i in goal)
+    state = State(table, goal_state)
     
-    show(method, table, size)
+    start_time = time.time()
+    feedback = bfs(State(table, goal_state))
+    end_time = time.time()
 
-    final = bfs(table, ','.join(str(i) for i in [0, 1, 2, 3, 4, 5, 6, 7, 8]), size)
-    print(final)
+    current_state = feedback["state"]
+    # {"state": current_state, "max_depth": max_depth, "max_frontier": max_frontier, "final_frontier": len(queue)}
+    print(f"Final State Found: {current_state is not None}")
+    if current_state is not None:
+        print(f"Path             :{current_state}")
+        print(f"Cost             : {current_state.cost}")
+        print(f"Depth            : {current_state.depth}")
+    print(f"Max_Depth        : {feedback['max_depth']}")
+    print(f"Final_Frontier   : {feedback['final_frontier']}")
+    print(f"Max_Frontier     : {feedback['max_frontier']}")
+    print(f"Scanned          : {feedback['scanned']}")
+    print(f"Elapsed_Time     : {end_time - start_time} s")
+    print(f"RAM_Usage        : {resource.getrusage(resource.RUSAGE_SELF).ru_maxrss/1000.0} kb")
 
-    final = bfs(table, ','.join(str(i) for i in [1, 2, 3, 4, 5, 6, 7, 8, 0]), size)
-    print(final)
 
-    final = bfs(table, ','.join(str(i) for i in [1, 2, 3, 8, 0, 4, 7, 6, 5]), size)
-    print(final)
+    #traceback_board(current_state)
 
+
+
+
+
+
+
+    
     return
 
 
